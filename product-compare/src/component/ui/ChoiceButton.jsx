0aee150e8fd902883fa11eb709fca2df0d2a627a
gyural <dla93190@naver.com>
2023-08-07 18:25:16 +0900

    상품 선택화면 1 구현 완료!!!

---
+++ product-compare/src/component/ui/ChoiceButton.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components"
+import image1 from "../../images/north_west_v2 (1).png"
+
+
+const Wrapper = styled.div`
+    width: 94px;
+    height: 94px;
+    padding: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 70px;
+    background-color: #05A929;
+    img{
+        height: 80%;
+        width: 80%;
+    }
+
+`
+function ChoiceButton (){
+
+    return(
+        <Wrapper>
+            <img 
+                style={{
+                    opacity:'0.4',
+                }}
+            src={image1} alt="선택 버튼 이미지" />
+        </Wrapper>
+    )
+
+}
+
+export default ChoiceButton;
