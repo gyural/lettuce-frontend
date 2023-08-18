a768eb055ff967836eeab623c501a3741cb62fde
gyural <dla93190@naver.com>
2023-08-18 15:19:06 +0900

    프록시 설정 완료

--- product-compare/src/setupProxy.js
+++ product-compare/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
       target: 'https://search.shopping.naver.com/catalog',
       changeOrigin: true, 
       pathRewrite: { 
-        // 요청 경로에서 /api/catalog 제거
+        // 요청 경로에서 /api/catalog 제거하기
         '^/api/catalog': '',
       },
     }),
